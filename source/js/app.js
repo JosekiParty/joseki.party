ac0f508e3717268f00e99f674bc75312efc22185
Nikolas Wise <nikolaswise@gmail.com>
2016-04-18 13:04:09 -0700

    m

--- source/js/app.js
+++ source/js/app.js
@@ -1,14 +1,6 @@
 import bus from './bus'
-import parseRoute from './routing'
 import model from './model'
+import view from './view'
 import intent from './intent'
 import render from './render'
-
-// Where are we tho?
-// parseRoute()
-
-// set up the model
-// model()
-
-bus.on('game:new', game => console.log(game))
-
+import route from './routing'
