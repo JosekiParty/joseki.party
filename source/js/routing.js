3f0aa480aae906bb0874a71e5815001995d213f2
Nikolas Wise <nikolaswise@gmail.com>
2016-04-18 13:04:01 -0700

    routes fire events

--- source/js/routing.js
+++ source/js/routing.js
@@ -1,27 +1,37 @@
 import routeMatcher from 'route-matcher'
 let match = routeMatcher.routeMatcher
-
 import bus from './bus'
 /**
 * Parse URL and navigate to correct pane/state
 */
-function parseRoute () {
-  let url = document.location.pathname + '/'
-  url = url.replace('//', '/')
+let url = document.location.pathname + '/'
+url = url.replace('//', '/')
 
-  let home = match('/').parse(url)
-  let watching = match('/:game/').parse(url)
-  let playing = match('/:game/:color/').parse(url)
+let home = match('/').parse(url)
+let watching = match('/:game/').parse(url)
+let playing = match('/:game/:color/').parse(url)
 
-  if (home) {
-    bus.emit('view:home')
-  } else if (watching) {
-    bus.emit('view:watching', watching)
-  } else if (playing) {
-    bus.emit('view:playing', playing)
-  } else {
-    bus.emit('view:404')
-  }
+if (home) {
+  bus.emit('view:set', {
+    section: 'home'
+  })
+} else if (watching && watching.game == 'new') {
+  console.log('new game plz')
+  bus.emit('view:set', {
+    section: 'new'
+  })
+} else if (watching) {
+  bus.emit('view:set', {
+    section: 'game',
+    watching: watching
+  })
+} else if (playing) {
+  bus.emit('view:set', {
+    section: 'game',
+    playing: playing
+  })
+} else {
+  bus.emit('view:set', {
+    section: 404
+  })
 }
-
-export default parseRoute
