106d0eb8f571caf91c81ed97eb5f1dc7420cb216
Nikolas Wise <nikolaswise@gmail.com>
2016-04-19 14:38:12 -0700

    hydrater

--- source/js/lib/hydrate-game.js
+++ source/js/lib/hydrate-game.js
@@ -2,5 +2,12 @@ import Weiqi from 'weiqi'
 
 export default function hydrate (game) {
   var Game = Weiqi.createGame(game.size)
+  for (let i = 0; i < game.history.length; i++) {
+    if (game.history[i]) {
+      var coords = [game.history[i].y, game.history[i].x]
+      var color = game.history[i].color
+      Game = Game.play(Weiqi[color], coords)
+    }
+  }
   return Game
 }
